16aa1b365f6fb2518da2b90c341f8144440b752b
Kevin Huang <yellow.khaos@gmail.com>
2019-02-04 03:00:39 +0000

    Automatically backed up by Learn

--- src/components/GifListContainer.js
+++ src/components/GifListContainer.js
@@ -1,4 +1,6 @@
 import React, {Component} from 'react';
+import GifList from './GifList';
+import GifSearch from './GifSearch';
 
 export default class GifListContainer extends Component {
   constructor() {
@@ -8,17 +10,26 @@ export default class GifListContainer extends Component {
     }
   }
 
-  searchHandle(query) {
+  searchHandle = (query) => {
+    
     let url = `http://api.giphy.com/v1/gifs/search?q=${query}&api_key=dc6zaTOxFJmzC&rating=g`
     fetch(url)
     .then(response => response.json())
-    .then(({gifs}) => this.setState({gifList : gifs}))
+    .then(gifs => {
+      this.setState({
+        gifList: gifs.data
+      })
+    })
   }
+
 
   render() {
 
     return (
-
+      <div>
+        <GifSearch searchSubmit={this.searchHandle}/>
+        <GifList gifs={this.state.gifList}/>
+      </div>
     )
 
   }
