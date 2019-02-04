cd115a4500b47ca9cb4d3c39b3928cf091bdbb51
Kevin Huang <yellow.khaos@gmail.com>
2019-02-04 03:01:44 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -2,4 +2,15 @@ import React, {Component} from 'react';
 
 export default class GifList extends Component {
 
+  
+
+  render() {
+
+    return (
+      <ul>
+        {this.props.gifs.map((gif, index) => <li><img src={gif.images.original.url} alt={gif.slug}/></li>)}
+      </ul>
+    )
+  }
+
 }
